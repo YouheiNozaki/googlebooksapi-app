1129d5341a7fb0d133693fe77319574f105adeb9
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-01-17 17:32:56 +0900

    cerate header-component

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>{{ title }}</header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+  props: {
+    title: String,
+  },
+});
+</script>
+
+<style scoped></style>
