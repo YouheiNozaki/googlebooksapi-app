636f7296a971df7d247e515a540b8019dd0a983f
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-01-19 22:22:46 +0900

    fix

---
+++ src/components/BookList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="book-list">
+    <BookItem v-for="book in books" :key="book.id" :book="book" />
+  </div>
+</template>
+
+<script lang="ts">
+import BookItem from "./BookItem.vue";
+import { defineComponent, PropType } from "vue";
+import type { Books } from "@/types/book";
+
+export default defineComponent({
+  props: {
+    books: Object as PropType<Books>,
+  },
+
+  components: {
+    BookItem,
+  },
+});
+</script>
+
+<style></style>
