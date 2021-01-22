4cd9dd9074f0bdfe50fd95f23f0a2509b66a3d95
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-01-22 22:30:21 +0900

    styled bookItem

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,25 +1,35 @@
 <template>
-  <section>
+  <section class="card m-6">
     <a :href="book?.volumeInfo.infoLink">
       <template v-if="book.volumeInfo.imageLinks">
-        <img
-          :src="book?.volumeInfo.imageLinks.thumbnail"
-          :alt="book.volumeInfo.title"
-        />
+        <div class="card-image">
+          <figure class="image">
+            <img
+              :src="book?.volumeInfo.imageLinks.thumbnail"
+              :alt="book.volumeInfo.title"
+            />
+          </figure>
+        </div>
       </template>
       <template v-else>
-        <img
-          src="@/assets/img/noimage.png"
-          :alt="book.volumeInfo.title"
-          width="128"
-        />
+        <div class="card-image">
+          <figure class="image">
+            <img
+              src="@/assets/img/noimage.png"
+              :alt="book.volumeInfo.title"
+              width="128"
+            />
+          </figure>
+        </div>
       </template>
-      <h4>{{ book?.volumeInfo.title }}</h4>
-      <p>{{ book.volumeInfo.publishedDate }}</p>
-      <span v-for="(author, index) in book.volumeInfo.authors" :key="index">
-        <em> {{ author }} </em>
-      </span>
-      <p>{{ book.volumeInfo.description }}</p>
+      <div class="card-content">
+        <h4 class="title">{{ book?.volumeInfo.title }}</h4>
+        <span v-for="(author, index) in book.volumeInfo.authors" :key="index">
+          <em> {{ author }} </em>
+        </span>
+        <p>{{ book.volumeInfo.publishedDate }}</p>
+        <p class="card-description">{{ book.volumeInfo.description }}</p>
+      </div>
     </a>
   </section>
 </template>
@@ -36,3 +46,12 @@ export default defineComponent({
   },
 });
 </script>
+<style scoped>
+.card-description {
+  margin-top: 2rem;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  overflow: hidden;
+}
+</style>
